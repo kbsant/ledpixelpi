5190bbc03fe6b736923814207c6a044663368992
kbsant <keansant@gmail.com>
2023-01-01 11:40:35 +0900

    wip pixel editor web app

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route('/')
+def index():
     return '<html><body><a href="static/paint.html">led pixel pi</p></body></html>'
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/px/<index>/<red>/<green>/<blue>')
+def px(index, red, green, blue):
+    #return f"Hello, i={escape(index)} r={escape(red)} g={escape(green)} b={escape(blue)}!"
+    return f'Hello, i={index} r={red} g={green} b={blue}!'
