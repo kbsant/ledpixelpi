0c2da4eaaa50417688493ce194f0191282bba687
kbsant <keansant@gmail.com>
2023-01-03 13:54:14 +0900

    wip pixel editor web app

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ from markupsafe import escape
 #import board
 #import neopixel
 
-#pixels = neopixel.NeoPixel(board.D18, 64)
-
-
+factor = 8
+width = 8
+height = 8
+size = width * height
+#pixels = neopixel.NeoPixel(board.D18, size)
 app = Flask(__name__)
+
+"""Translate from an array index to a board position, where pixels are strewn like a string"""
+def board_pos(i):
+    row = i // width
+    col = i % width
+    if row %2 == 1:
+        # for odd rows, count columns from the left
+        return (row * width) + col
+    # for even rows, count columns from the right
+    return (row * width) + width - col - 1
+
 
 @app.route('/')
 def index():
@@ -14,5 +27,6 @@ def index():
 
 @app.route('/px/<int:index>/<int:red>/<int:green>/<int:blue>')
 def px(index, red, green, blue):
-    #pixels[int(index)] = (int(red)/4,int(green)/4,int(blue)/4)
-    return f'Hello, i={index} r={red} g={green} b={blue}!'
+    pos = board_pos(index)
+    #pixels[pos] = (red//factor,green//factor,blue//factor)
+    return f'Hello at i={index} p={pos} r={red} g={green} b={blue}!'
